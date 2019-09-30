ff4e182e9fcece4528a24b4ce6e4fa9938ddb9aa
silvio <silvio.bentzien@gmx.de>
2019-10-01 01:06:13 +0200

    added debug flag; removed shared multiprocessing objects: updater should now work on windows

--- update_wow_addons.py
+++ update_wow_addons.py
@@ -1,7 +1,8 @@
 from configparser import ConfigParser
-from multiprocessing import Manager, Pool, Value
+from multiprocessing import Pool
 from os import cpu_count, mkdir
 from os.path import dirname, expanduser, getsize, isdir, isfile, join as pjoin
+from random import randint
 from time import time
 from zipfile import ZipFile
 
@@ -13,37 +14,40 @@ from bs4 import BeautifulSoup as bs
 
 class Updater:
     def __init__(self):
-        self.overwrite = False
+        self.debug = False  # changes game dir and always updates some random addons
+
+        self.cache_dir = pjoin(expanduser('~'), '.cache', 'wow-addon-updates')
+        if not isdir(self.cache_dir):
+            if not isdir(dirname(self.cache_dir)):
+                mkdir(dirname(self.cache_dir))
+            mkdir(self.cache_dir)
+
         self.config_file = pjoin(dirname(__file__), 'update_wow_addons.config')
         self.config = ConfigParser(allow_no_value=True, interpolation=None)
-
-        if not isfile(self.config_file):
-            exit(f'Error: Config file \'{self.config_file}\' not found.')
+        assert isfile(self.config_file), exit(f'Error: Config file \'{self.config_file}\' not found.')
 
         with open(self.config_file, 'r') as f:
             self.config.read_file(f)
 
         self.game_version = self.config['settings']['version'].lower()
-        if self.game_version not in ['classic', 'retail']:
-            exit(f'Error: Game version \'{self.game_version}\' not recognized.'
-                 f'Must be either \'classic\' or \'retail\'.')
+        assert self.game_version in ['classic', 'retail'], exit(f'Error: Game version \'{self.game_version}\' not recognized. Must be either \'classic\' or \'retail\'.')
 
         self.game_dir = self.config['settings']['game directory']
-        if not isdir(self.game_dir):
-            exit(f'Error: \'{self.game_dir}\' is not a directory.')
+        assert isdir(self.game_dir), exit(f'Error: \'{self.game_dir}\' is not a directory.')
+
+        if self.debug:
+            debug_dir = '/home/silvio/tmp/updater_test'
+            self.game_dir = debug_dir
+            print(f'Running in debug mode. Changing game directory to \'{debug_dir}\' and updating random addons.')
 
         self.addon_dir = pjoin(self.game_dir, f'_{self.game_version}_', 'Interface', 'AddOns')
-        if not isdir(self.addon_dir):
-            exit(f'Error: \'{self.addon_dir}\' is not a directory.')
+        assert isdir(self.addon_dir), exit(f'Error: \'{self.addon_dir}\' is not a directory.')
 
-        self.cache_dir = pjoin(expanduser('~'), '.cache', 'wow-addon-updates')
-        if not isdir(self.cache_dir):
-            mkdir(self.cache_dir)
-
-        self.addons = Manager().dict({})
+        self.addons = {}
+        self.size = 0.0
 
         for name, last_update in self.config.items(self.game_version):
-            if last_update is None or self.overwrite:
+            if last_update is None or (self.debug and bool(randint(0, 1))):
                 last_update = 0.0
             self.addons[name] = float(last_update)
 
@@ -63,8 +67,8 @@ class Updater:
         soup = bs(r.text, 'html.parser')
         rows = soup.find_all('tr')
 
-        self.print_looking_for_update(idx.value)
-        idx.value += 1
+        # self.print_looking_for_update(idx.value)
+        # idx.value += 1
 
         for row in rows[1:]:
             cols = row.find_all('td')
@@ -73,11 +77,10 @@ class Updater:
                 last_update_curse = int(cols[3].find('abbr').get('data-epoch'))
                 if last_update_curse > self.addons[addon_name]:
                     file_url = cols[1].find('a')['href']
-                    updateable[addon_name] = file_url
-                    break
+                    return addon_name, file_url
 
-    def update_addon(self, addon_name):
-        file_url = updateable[addon_name]
+    def update_addon(self, tup):
+        addon_name, file_url = tup
         addon_start = time()
         out_path = pjoin(self.cache_dir, f'{addon_name}.zip')
         cfs = cfscrape.create_scraper()
@@ -95,9 +98,8 @@ class Updater:
                     break
 
         ZipFile(out_path).extractall(self.addon_dir)
-        updated[addon_name] = addon_start
         zip_size = getsize(out_path) / 1024 / 1024
-        size.value += zip_size
+        return addon_name, addon_start, zip_size
 
     def print_looking_for_update(self, idx, eol=' '):
         anim = ['⠶', '⠦', '⠖', '⠲', '⠴']
@@ -112,11 +114,19 @@ class Updater:
         init()
         start = time()
 
+        self.print_looking_for_update(idx=0, eol='\n\n')
+
         # check for lates versions
         with Pool(num_workers) as p:
-            p.map(self.find_update, self.addons)
+            re = p.map(self.find_update, self.addons)
 
-        self.print_looking_for_update(idx=0, eol='\n\n')
+        keys = []
+        updateable = []
+        for i in re:
+            if i:
+                addon_name, file_url = i
+                keys.append(addon_name)
+                updateable.append((addon_name, file_url))
 
         # find_update populates updateable
         updateable_len = len(updateable)
@@ -129,31 +139,25 @@ class Updater:
             print(f'{Style.BRIGHT}{Fore.CYAN}=>{Fore.RESET}'
                   f' Updating {Fore.YELLOW}{updateable_len if updateable_len > 1 else ""}'
                   f'{Fore.RESET}{" addons" if updateable_len > 1 else "addon"}:{Style.RESET_ALL}'
-                  f'{Fore.LIGHTGREEN_EX}', ' '.join(sorted(updateable.keys())), Fore.RESET, '\n')
+                  f'{Fore.LIGHTGREEN_EX}', ' '.join(sorted(keys)), Fore.RESET, '\n')
 
             tqdm.get_lock()  # ensures locks exist
 
             # update out-of-date addons
             with Pool(num_workers) as p:
-                for _ in tqdm(p.imap_unordered(self.update_addon, updateable), total=updateable_len):
-                    pass
-
-            for addon_name, timestamp in updated.items():
-                self.config[self.game_version][addon_name] = str(timestamp)
+                for addon, ts, size in tqdm(p.imap_unordered(self.update_addon, updateable), total=updateable_len):
+                    self.size += size
+                    self.config.set(f'{self.game_version}', addon, str(ts))
 
             with open(self.config_file, 'w') as f:
                 self.config.write(f)
 
-            print(f'\nsummary: {round(time() - start, ndigits=2)}s, {round(size.value, ndigits=2)}MB')
+            print(f'\nsummary: {round(time() - start, ndigits=2)}s, {round(self.size, ndigits=2)}MB')
 
         deinit()
 
 
 if __name__ == '__main__':
-    updateable = Manager().dict({})
-    updated = Manager().dict({})
-    size = Value('d', 0.0)
-    idx = Value('i', 0)
-
+    # idx = Value('i', 0)
     wup = Updater()
     wup.main()
